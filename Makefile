6638d5b0e2e585c9c6de0ffa416ce5edebea5481
alexander <alexzund3r@yahoo.com>
2016-03-14 16:17:46 +0600

    Args handling

--- Makefile
+++ Makefile
@@ -6,5 +6,20 @@ build:
 	gcc -o $(OUT)/main.o $(SRC)/main.c;
 
 run:
-	./$(OUT)/main.o 
+	./$(OUT)/main.o
+
+run-std:
+	./$(OUT)/main.o --mode=std
+
+run-child:
+	./$(OUT)/main.o --mode=child
+    
+run-kill:
+	./$(OUT)/main.o --mode=kill
+    
+run-posix:
+	./$(OUT)/main.o --mode=posix
+
+run-pipe:
+	./$(OUT)/main.o --mode=pipe    
     
--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
- #include "stdio.h" 
- #include "stdlib.h" 
- #include "string.h" 
- #include "sys/signal.h" 
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/signal.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <getopt.h>
 
 void sigusronehandler(int signum, siginfo_t* info, void* f) {
     printf("We tried access %p, ", info->si_addr);
@@ -9,15 +12,49 @@ void sigusronehandler(int signum, siginfo_t* info, void* f) {
 
 
 
-int main(int argc, char* argv[]) {
-    struct sigaction act;
+int main(int argc, char** argv) {
+    
+    int c;
+    char* mode;
+    
+    while (1) {
+        int option_index = 0;
+        
+        static struct option long_options[] = {
+             {"mode", required_argument, 0,  0 },
+        };
+        
+        
+        c = getopt_long(argc, argv, "m",
+                        long_options, &option_index);
+        
+        if (c == -1)
+            break;
+        else {
+            mode = optarg;
+        }
+    }
+    
+    if (strcmp(mode, "pipe") == 0) {
+        printf("Pipe mode activated\n");
+    } else if (strcmp(mode, "child") == 0) {
+        printf("Child mode activated\n");
+    } else if (strcmp(mode, "std") == 0) {
+        printf("Std mode activated\n");
+    } else if (strcmp(mode, "kill") == 0) {
+        printf("Kill mode activated\n");
+    } else if (strcmp(mode, "posix") == 0) {
+        printf("POSIX mode activated");
+    }
+    
+ /*   struct sigaction act;
     
     memset(&act, 0, sizeof(act));
     act.sa_sigaction = sigusronehandler;
     act.sa_flags = SA_SIGINFO;
     sigaction(SIGUSR1, &act, NULL);
     
-   // raise(SIGUSR1);
+   // raise(SIGUSR1); */
     
     
     return 0;
